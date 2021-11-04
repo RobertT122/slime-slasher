14a233fe7269e9240c8d0945925b87ad8172760f
RobertT122 <robert@tetrault.org>
2021-11-04 15:44:23 -0400

    Alter Aspect Ratio Thresholds

--- src/index.js
+++ src/index.js
@@ -22,12 +22,12 @@ window.addEventListener("DOMContentLoaded", () =>{
     let aspectRatio = docWidth/docHeight;
     let x = event.pageX;
     let y = event.pageY;
-    if (docHeight > 1080 && aspectRatio > 3/4){
+    if (docHeight > 1080 && aspectRatio > 5/6){
       console.log("hi-rez")
       //working
       let margin = (docWidth-800)/2
       x -= margin;
-    }else if (aspectRatio > 3/4){
+    }else if (aspectRatio > 5/6){
       // console.log("window");
       //working
       let margin = (800/600)*(docWidth-600)/2
@@ -35,7 +35,7 @@ window.addEventListener("DOMContentLoaded", () =>{
       x = Math.round(scaler * x) - margin;
       y = Math.round(scaler * y);
       console.log(`x:${x}, y:${y}`)
-    }else if (aspectRatio >9/16){
+    }else if (aspectRatio >5/8){
       //working
       // console.log("ipad")
       let margin = (docWidth*.1)
@@ -72,12 +72,12 @@ window.addEventListener("DOMContentLoaded", () =>{
     console.log("hello")
     if (docHeight > 1080 && aspectRatio > 3/4){
       ctx.fillText("Hi-Rez", 320, 1400)
-    }else if (aspectRatio > 3/4){
+    }else if (aspectRatio > 5/6){
       ctx.fillText("Window/Landscape", 200, 1400)
-    }else if (aspectRatio >9/16){
+    }else if (aspectRatio >5/8){
       ctx.fillText("Tablet", 320, 1400)
     }else{
-      ctx.fillText("Mobile", 320, 1400)
+      ctx.fillText("Mobile", 400, 1400)
     }
 
 
